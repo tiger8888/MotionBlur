0a5ba54ed23299182c4f5ae065cadab776c2ddf6
Guanshan Liu <guanshan.liu@gmail.com>
2014-10-18 15:33:58 +0800

    view controller, storyboard auto layout config has not finished

--- MotionBlur/ViewController.swift
+++ MotionBlur/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var topMenu: UIImageView!
+    @IBOutlet weak var topMenuHiddenConstraint: NSLayoutConstraint!
+    @IBOutlet weak var toggleButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        topMenuHiddenConstraint.priority = 1000
+
+        toggleButton.setTitle("Creating...", forState: .Normal)
+        toggleButton.enabled = false
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+
+        weak var weakSelf = self
+        self.topMenu.enableMotionBlur(Float(M_PI_2)) {
+            let strongSelf = weakSelf
+            strongSelf?.toggleButton.setTitle("Toggle", forState: .Normal)
+            strongSelf?.toggleButton.enabled = true
+        }
+    }
+
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(animated)
+
+        topMenu.disableMotionBlur()
+    }
+
+    @IBAction func move(sender: AnyObject) {
+        topMenuHiddenConstraint.active = !topMenuHiddenConstraint.active
+
+        let hiding = topMenuHiddenConstraint.active
+
+        UIView.animateWithDuration(0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: hiding ? 0 : 0.6, options: .AllowUserInteraction | .BeginFromCurrentState, animations: { () -> Void in
+            self.view.backgroundColor = hiding ? UIColor(white: 0.907, alpha: 1) : UIColor(white: 0.8, alpha: 1)
+            self.topMenu.superview?.layoutIfNeeded()
+        }, completion: nil)
+    }
 
 }
 
